555b53d5c6726c119a3aba9f3ed23660d1ba3b9c
魏子博 <290008658@qq.com>
2026-09-23 07:18:27 +0800

    fix(java): hard-disable ops admin data autoconfig

--- phyok-java/apps/ops-admin-service/src/main/java/com/phyok/opsadmin/OpsAdminServiceApplication.java
+++ phyok-java/apps/ops-admin-service/src/main/java/com/phyok/opsadmin/OpsAdminServiceApplication.java
@@ -3,7 +3,16 @@ package com.phyok.opsadmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.phyok")
+@SpringBootApplication(
+    scanBasePackages = "com.phyok",
+    excludeName = {
+        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
+        "org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration",
+        "org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration",
+        "org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration",
+        "org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration"
+    }
+)
 public class OpsAdminServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OpsAdminServiceApplication.class, args);
