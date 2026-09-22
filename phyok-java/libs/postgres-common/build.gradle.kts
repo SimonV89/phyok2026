eec73e9e162c7f4dcbfc60d9a2ec445853922fc0
魏子博 <290008658@qq.com>
2026-09-23 01:17:32 +0800

    fix(java): add flyway postgres module

--- phyok-java/libs/postgres-common/build.gradle.kts
+++ phyok-java/libs/postgres-common/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     api(project(":libs:mybatis-common"))
     api("org.postgresql:postgresql")
     api("org.flywaydb:flyway-core")
+    api("org.flywaydb:flyway-database-postgresql")
 }
